e04a5f8f0fa2085024ff35572c78d28d55ebb44e
ryanjhbae <59541976+ryanjhbae@users.noreply.github.com>
2021-09-18 22:41:07 -0400

    small changes to calendar

--- front-end/src/components/Calendar.js
+++ front-end/src/components/Calendar.js
@@ -1,16 +1,26 @@
 import React from "react";
 import Calendar from "@ericz1803/react-google-calendar"
 import { css } from "@emotion/react";
-import { LANGUAGE, HOLIDAYS_IN_CANADA, GOOGLE_API_KEY, GOOGLE_CLIENT_ID } from "../config";
+import { LANGUAGE, HOLIDAYS_IN_CANADA, UWATERLOO_IMPORTANT_DATES,
+        GOOGLE_API_KEY, GOOGLE_CLIENT_ID } from "../config";
 
 let calendars = [
-    {calendarId: HOLIDAYS_IN_CANADA}
+    {calendarId: HOLIDAYS_IN_CANADA, color: "#B241D1"},
+    {calendarId: UWATERLOO_IMPORTANT_DATES, color: "yellow"}
 ];
 
 let styles = {
     //you can use object styles (no import required)
     calendar: {
       borderWidth: "3px", //make outer edge of calendar thicker
+    },
+    today: css`
+    /* highlight today by making the text red and giving it a red border */
+        color: red;
+        border: 1px solid red;
+    `,
+    event: {
+        color: "blue"
     }
 };
 
--- front-end/src/config.js
+++ front-end/src/config.js
@@ -3,5 +3,6 @@ export const GOOGLE_CLIENT_ID = '832600522932-0urudifhjb69ap6imc2hl7imb9k27mo7.a
 export const GOOGLE_API_KEY = 'AIzaSyAscF9WCaJOV8hU6kzXcFugZSAIIrR1PQU';
 export const COUNTRY = "ca";
 export const DEFAULT_ARTICLE_NUM = 4;
-export const HOLIDAYS_IN_CANADA = 'canadian.official#holiday@group.v.calendar.google.com';
+export const HOLIDAYS_IN_CANADA = "en.canadian.official#holiday@group.v.calendar.google.com";
+export const UWATERLOO_IMPORTANT_DATES = "cjv74dvfs8gdtp202340m8b3fs@group.calendar.google.com";
 export const LANGUAGE = "EN";

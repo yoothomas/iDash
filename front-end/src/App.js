f7581e9cc1a01808daa4e32dcc4b234050715621
ryanjhbae <59541976+ryanjhbae@users.noreply.github.com>
2021-09-18 23:33:27 -0400

    merge resolved?

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,19 +1,42 @@
 import Search from './components/Search.js';
 import './App.css';
 import BasicGrid from './components/BasicGrid';
-import React from 'react';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import { useAuthState } from 'react-firebase-hooks/auth';
 
-class App extends React.Component {
+const provider = new GoogleAuthProvider();
 
-    render() {
-        return (
-            <div className="App">
-                <h1 className="text-center mb-4">Welcome :)</h1>
-                <Search />
-                <BasicGrid />
-            </div>
-      );
-    } 
+const firebaseConfig = {
+  apiKey: "AIzaSyAscF9WCaJOV8hU6kzXcFugZSAIIrR1PQU",
+  authDomain: "hackthenorth-326415.firebaseapp.com",
+  projectId: "hackthenorth-326415",
+  storageBucket: "hackthenorth-326415.appspot.com",
+  messagingSenderId: "832600522932",
+  appId: "1:832600522932:web:01cb0fae9cb8367eb617e1",
+  measurementId: "G-G2NKFGG2M0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+//Initialize Authorization
+const auth = getAuth();
+const [user] = useAuthState(auth);
+
+function App () {
+    return (
+        <div className="App">
+            <h1 className="text-center mb-4">Welcome :)</h1>
+            <Search />
+            <section>
+                {user ? <BasicGrid /> : <SignIn />}
+            </section>
+            <BasicGrid />
+        </div>
+    );
 }
 
 export default App;
